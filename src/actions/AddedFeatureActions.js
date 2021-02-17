90fa582bce5245692eaa4c77f92c692d9c708711
Beau Jennings <redalthazar@gmail.com>
2021-02-16 19:47:51 -0600

    added reducer and action for addedFeatures.

---
+++ src/actions/AddedFeatureActions.js
@@ -0,0 +1,11 @@
+export const ADD_FEATURE = "ADD_FEATURE";
+
+export const addFeature = (feature, price) => {
+    return ({
+        type: ADD_FEATURE,
+        payload: {
+            feature: feature,
+            price: price
+    }
+    });
+};
